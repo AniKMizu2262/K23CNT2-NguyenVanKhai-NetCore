2c2f1af365a3b228f275dad3328dd9040019b585
Nguyễn Văn Khải <khainguyenangelz10072005@gmail.com>
2025-04-22 10:34:50 +0700

    .NET CORE Lesson 01 Mvc

---
+++ nvk-Lesson01Mvc/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nvk_Lesson01Mvc.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
