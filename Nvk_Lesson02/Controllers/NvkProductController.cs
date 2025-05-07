bca9b5e048c39e16dcd88492abd0570b2f3734dc
Nguyễn Văn Khải <khainguyenangelz10072005@gmail.com>
2025-05-07 15:59:30 +0700

    .NET CORE Lesson 02

---
+++ Nvk_Lesson02/Controllers/NvkProductController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nvk_Lesson02.Controllers
+{
+    public class NvkProductController : Controller
+    {
+        public IActionResult NvkIndex()
+        {
+            ViewData["messageData"] = "Dữ liệu từ viewData";
+            ViewBag.messageData = "Dữ liệu từ viewBag";
+            TempData["messageData"] = "Dữ liệu từ tempData";
+            return View();
+        }
+    }
+}
